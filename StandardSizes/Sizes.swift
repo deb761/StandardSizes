67ff8eb1b4e3c28557c0e8a8fd695cc5f41c6384
Deborah Engelmeyer <dengelmeyer@icloud.com>
2016-08-26 15:32:43 -0700

    Successfully created method to save sweater and vest areas to a user selected file.

--- StandardSizes/Sizes.swift
+++ StandardSizes/Sizes.swift
@@ -7,21 +7,39 @@
 //
 
 import Foundation
+import Cocoa
 
 public class Sizes {
-    private let charts = ["baby", "child", "youth", "women", "men"]
+    private let charts = ["Baby", "Child", "Youth", "Women", "Men"]
     /// An array of keyed rows with the data for each size
     public var sizes = [[String: Double]]()
-    private let root = "/Users/deb/Documents/workspace/StandardSizes/StandardSizes/StandardSizes/data"
-    private let hipLength = ["baby" : 2.5, "child" : 5, "youth" : 10, "women" : 15, "men" : 10]
+    private let hipLength = ["Baby" : 2.5, "Child" : 5, "Youth" : 10, "Women" : 15, "Men" : 10]
     
     public init() {
         for chart in charts {
-            let fileContent = try? NSString(contentsOfFile: "\(root)/\(chart).csv", encoding: NSUTF8StringEncoding)
-            let lines = fileContent!.componentsSeparatedByString("\r\n")
+            
+            var status = ""
+            guard let asset = NSDataAsset(name: chart) else {
+                status = "Could not find the data"
+                return
+            }
+        
+            let options = [
+                NSDocumentTypeDocumentAttribute : NSPlainTextDocumentType,
+                NSCharacterEncodingDocumentAttribute : NSUTF8StringEncoding
+                ] as [String : AnyObject]
+            
+            var fileContent:NSAttributedString?
+            do {
+                fileContent = try NSAttributedString(data: asset.data, options: options, documentAttributes: nil)
+            } catch let err{
+                status = "Error = \(err)"
+            }
+        
+            let lines = fileContent?.string.componentsSeparatedByString("\r\n")
             var first = true
             var cols = [String]()
-            for line in lines {
+            for line in lines! {
                 let toks = line.componentsSeparatedByString(",")
                 if (first) {
                     cols = toks
@@ -32,7 +50,7 @@ public class Sizes {
                         row[cols[idx]] = Double(toks[idx])
                     }
                     switch chart {
-                    case "men":
+                    case "Men":
                         row["Back Waist Length"] = row["Back Hip Length"]! - hipLength[chart]!
                     default:
                         row["Back Hip Length"] = row["Back Waist Length"]! + hipLength[chart]!
@@ -52,39 +70,35 @@ public class Sizes {
      71 <= size < 110   12.5cm
         >= 110          15cm */
     public func calcSweaterAreas() {
-        let file = "sweaters.csv" //this is the file. we will write to and read from it
-        
-        if let dir = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.AllDomainsMask, true).first {
-            let path = NSURL(fileURLWithPath: dir).URLByAppendingPathComponent(file)
-            
-            //writing
-            do {
-                try text.writeToURL(path, atomically: false, encoding: NSUTF8StringEncoding)
-            }
-            catch {/* error handling here */}
-            
-            //reading
-            do {
-                let text2 = try NSString(contentsOfURL: path, encoding: NSUTF8StringEncoding)
-            }
-            catch {/* error handling here */}
-        }        let path = root.URLByAppendingPathComponent("")
-        let text = "some text"
-        
-        //writing
-        text.writeToFile(path, atomically: false, encoding: NSUTF8StringEncoding, error: nil);
-
         for idx in 0..<sizes.count {
             var size = sizes[idx]
             let chest = eased(size["Chest"]!)
             let body = size["Back Hip Length"]! * chest
             let sleeves = chest * 0.75 * (size["Neck-to-Cuff"]! - chest)
             size["sweater-area"] = body + sleeves
+            size["vest-area"] = body
             sizes[idx] = size
         }
-    
+        
     }
     
+    /* Save the sweater and vest areas to a file */
+    public func saveAreas(url:NSURL) {
+        let hdr = "Chest,Sweater,Vest\r\n"
+        do {
+            try hdr.writeToURL(url, atomically: false, encoding: NSUTF8StringEncoding)
+            for size in sizes {
+            let str = "\(size["Chest"]!),\(size["sweater-area"]!),\(size["vest-area"]!)"
+            try str.appendLineToURL(url)
+        }
+        
+        } catch let error as NSError {
+            let myPopup = NSAlert(error: error)
+            myPopup.runModal()
+        }
+    }
+    
+    /* Calculate the finished chest size for a sweater or vest */
     private func eased(chest: Double) -> Double {
         var ease = 0.0
         switch chest {
--- StandardSizes/ViewController.swift
+++ StandardSizes/ViewController.swift
@@ -10,6 +10,7 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var nameField: NSTextField!
     var sizes:Sizes!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,5 +26,23 @@ class ViewController: NSViewController {
     }
 
 
+    @IBAction func saveData(sender: AnyObject) {
+        // get the values from the name and data fields
+        let docName = nameField.stringValue
+        //let docDataTextView = dataScrollView.contentView.documentView as! NSTextView
+        //let docData = docDataTextView.string!
+        
+        // create a Save Panel to choose a file path to save to
+        let dlg = NSSavePanel()
+        // use the name fields value to suggest a name for the file
+        dlg.nameFieldStringValue = docName
+        // run the Save Panel and handle an OK selection
+        if (dlg.runModal() == NSFileHandlingPanelOKButton) {
+            // get the URL of the selected file path
+            let saveUrl = dlg.URL
+            let fileUrlWithExt = saveUrl?.URLByAppendingPathExtension("csv")
+            sizes.saveAreas(fileUrlWithExt!)
+        }
+    }
 }
 
