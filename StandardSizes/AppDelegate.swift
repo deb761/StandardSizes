8f8af75f7717e20896b4441f8967ae2c0cd7d2fe
Deborah Engelmeyer <dengelmeyer@icloud.com>
2016-08-26 09:05:02 -0700

    Initial Commit

---
+++ StandardSizes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StandardSizes
+//
+//  Created by Deborah Engelmeyer on 8/26/16.
+//  Copyright © 2016 The Inquisitive Introvert. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ StandardSizes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StandardSizes
+//
+//  Created by Deborah Engelmeyer on 8/26/16.
+//  Copyright © 2016 The Inquisitive Introvert. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
